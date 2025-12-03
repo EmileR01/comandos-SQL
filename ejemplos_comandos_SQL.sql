ef3fca5c3cdb9f17be09b25427c48e998054b8ae
Emile Rengifo <rengifoemi3@gmail.com>
2025-12-03 16:38:59 -0600

    se hace uso de los diferentes comandos como crear, eliminar, modificar tablas en sql

--- ejemplos_comandos_SQL.sql
+++ ejemplos_comandos_SQL.sql
@@ -5,4 +5,40 @@ CREATE DATABASE testDB;
 CREATE DATABASE bdBorrar;
 -- DROP DATABASE borrar una base de datos 
 DROP DATABASE bdBorrar;
-
+USE testDB;
+-- CREATE TABLE para crear una tabla
+CREATE TABLE persons (
+	per_id int,
+	per_last_name varchar(255),
+	per_first_name varchar(255),
+	per_address varchar(255),
+	per_city varchar(255)
+);
+create table persons1 (
+	per_id int,
+	per_last_name varchar(255),
+	per_first_name varchar(255),
+	per_address varchar(255),
+	per_city varchar(255)
+    );
+    
+    -- DROP TABLE para borrar la tabla
+    drop table persons1;
+    
+    -- ALTER TABLE para agregar, eliminar o modificar columnas 
+    alter table  persons
+    add per_email varchar (255);
+     -- ALTER TABLE elimina una columna 
+	alter table persons
+    drop column per_email;
+    
+    -- ALTER TABLE renombrar una columna
+    ALTER TABLE persons
+    RENAME COLUMN per_address to per_phone;
+    -- ALTER TABLE cambiar tipo de dato
+    ALTER TABLE persons
+    MODIFY COLUMN per_phone varchar(15);
+    
+    
+    
+    
